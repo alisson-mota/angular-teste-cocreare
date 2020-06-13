6d59be3735792d23a13072891282e22e4724b853
alisson.mota <alisson.mota@fireworkweb.com>
2020-06-13 00:21:30 -0300

    feat(alunos): criando adicionado dados mokados

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -5,13 +5,42 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-alunos',
   template: `
-    <div *ngFor="let aluno of alunos$ | async"> 
-      {{ aluno.nome }}
-      {{ aluno.nota }}
+<style>
+#format td, #format th {
+  width: 80%;
+  border: 1px solid #ddd;
+  text-align: center;
+  padding:20px;
+}
+#format th {
+  background-color: #f2f2f2;
+}
+#format h3{
+  text-align: center;
+} 
+#format table {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+
+    <div id="format">
+      <h3>Informações dos alunos</h3>
+      <table>
+        <tr>
+          <th>Nomes</th>
+          <th>Notas</th>
+        </tr>
+        <tr>
+          <td> <div *ngFor="let aluno of alunos$ | async">
+            {{  aluno.nome }}</div></td>
+          <td><div *ngFor="let aluno of alunos$ | async">
+            {{ aluno.nota }}</div></td>
+        </tr>
+      </table>
     </div>
   `,
-  styles: [
-  ]
+
 })
 export class AlunosComponent implements OnInit {
   
